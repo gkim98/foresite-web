4b51e7ddd8a8c0a4430cfb73d984bc0d803ff611
daniel <daniel.zhou2012@gmail.com>
2018-09-08 03:37:49 -0400

    finished mock visualization

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import './App.css';
 import AppRouter from './routers/AppRouter';
-
 //test
 import MapPage from './pages/MapPage';
+import DataVisPage from './pages/DataVisPage';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <MapPage />
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="App">
+                <DataVisPage/>
+            </div>
+        );
+    }
 }
 
 export default App;
